0f158fbf2a6c83a30e1a55d60956ac844b5ad993
dylangavilan <dylan.gavilan32@gmail.com>
2024-06-20 19:10:15 -0300

    delete comments

--- src/components/inputs/input.jsx
+++ src/components/inputs/input.jsx
@@ -4,7 +4,6 @@ const Input = ({errorMessage, ...props}) => {
   return (
     <>
     <input
-        // className={}
         {...props}
     />
      {errorMessage && <span className='error-message'>{errorMessage}</span>} 
