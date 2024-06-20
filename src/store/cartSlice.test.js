1e9dedf3dfc7ca54d4e4ced4f1fb6773b113bb52
dylangavilan <dylan.gavilan32@gmail.com>
2024-06-20 17:39:39 -0300

    test jest

---
+++ src/store/cartSlice.test.js
@@ -0,0 +1,64 @@
+import cartReducer, {
+    addToCart,
+    handleOpen,
+    addMore,
+    lessOne,
+    removeFromCart,
+    clear,
+  } from './cartSlice';
+  
+  describe('cartSlice', () => {
+    const initialState = {
+      items: [],
+      isOpen: false,
+    };
+  
+    it('add to Cart', () => {
+      const newItem = { id: 1, name: 'Leche', amount: 5 };
+      const actual = cartReducer(initialState, addToCart(newItem));
+      expect(actual.items).toHaveLength(1);
+      expect(actual.items[0]).toEqual({ ...newItem, quantity: 1 });
+    });
+  
+    it('handleOpen', () => {
+      const actual = cartReducer(initialState, handleOpen());
+      expect(actual.isOpen).toEqual(true);
+    });
+  
+    it('addOne', () => {
+      const startState = {
+        items: [{ id: 1, name: 'Leche', amount: 5, quantity: 1 }],
+        isOpen: false,
+      };
+      const actual = cartReducer(startState, addMore(1));
+      expect(actual.items[0].quantity).toEqual(2);
+    });
+  
+    it('lessOne quantity', () => {
+      const startState = {
+        items: [{ id: 1, name: 'Leche', amount: 5, quantity: 2 }],
+        isOpen: false,
+      };
+      const actual = cartReducer(startState, lessOne(1));
+      expect(actual.items[0].quantity).toEqual(1);
+    });
+  
+    it('remobeById', () => {
+      const startState = {
+        items: [{ id: 1, name: 'Leche', amount: 5, quantity: 1 }, { id: 2, name: 'Arroz', amount: 5, quantity: 1 }],
+        isOpen: false,
+      };
+      const actual = cartReducer(startState, removeFromCart(1));
+      expect(actual.items).toHaveLength(1);
+    });
+  
+    it('clear cart', () => {
+      const startState = {
+        items: [{ id: 1, name: 'Leche', amount: 5, quantity: 1 }],
+        isOpen: false,
+      };
+      const actual = cartReducer(startState, clear());
+      expect(actual.items).toHaveLength(0);
+    });
+  });
+  
